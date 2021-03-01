3875f37b6271b73f8a451023160c627f60550ece
Ender Yasin Çüren <yasincuren-dev@outlook.com>
2021-03-01 22:49:23 +0300

    entity framework yapısı eklendi

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLİD
+    //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategory(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+        }
+    }
+}
