788f695213a9f8c5c79967591919d4a09bc4e831
Ender Yasin Çüren <yasincuren-dev@outlook.com>
2021-03-10 01:06:44 +0300

    Core katmanı eklendi ve gerekli iyileştirmeler yapıldı

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,11 +1,10 @@
-﻿
-using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // generic constrait
     // class: referans tip
